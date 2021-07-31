79a6f61b45543a856c2464f250421d0c45a6e639
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2021-07-31 10:33:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ screen = Screen()
 screen.setup(height=800, width=800)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
 paddle = Turtle()
 paddle.shape("square")
@@ -11,5 +12,22 @@ paddle.color("white")
 paddle.shapesize(stretch_len=1, stretch_wid=5)   # width is 5 x default ie 5 x 20 & len is 20
 paddle.penup()
 paddle.goto(350, 0)
+screen.update()
+
+
+def move_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def move_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()      # screen will listen to commands such as keystrokes
+screen.onkey(move_up, "up")
+screen.onkey(move_down, "down")
+
 
 screen.exitonclick()
